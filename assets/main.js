383e89497a82cea034c4f74673abf050c704d98b
Sunshine <35852811+sunschong@users.noreply.github.com>
2019-10-16 01:20:49 -0700

    added 1.0 files

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+// sidenav for mobile mode
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false, // remove the 3 dots
+  height: 500,
+  transition: 500,
+  interval: 8000 // 6 seconds
+});
+
+// scrollspy
+const scrollspy = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scrollspy, {});
+
+// parallax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax, {});
+
+// modal program
+const modalProgram = document.querySelector('#program');
+M.Modal.init(modalProgram, {});
+
+// modal resources
+const modalResources = document.querySelector('#resources');
+M.Modal.init(modalResources, {});
+
+const modalElizabeth = document.querySelector('#elizabeth_profile');
+M.Modal.init(modalElizabeth, {});
+
+const modalSam = document.querySelector('#sam_profile');
+M.Modal.init(modalSam, {});
+
+// and more modal 
+const modalMore = document.querySelector('#and_more');
+M.Modal.init(modalMore, {})
+
+// modal news and tabs
+const newsModal = document.querySelector('#newsModal');
+M.Modal.init(newsModal, {});
+const tabs = document.querySelector('.tabs');
+M.Tabs.init(tabs, {});
+const newsSelect = document.querySelector('select');
+M.FormSelect.init(newsSelect, {});
